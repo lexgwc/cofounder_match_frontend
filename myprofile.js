cd7550a2ad974b847c8238737da69963b9c45035
Alexa Clark <lex.gwc@gmail.com>
2024-04-11 15:54:08 -0400

    Fetch profile data for logged in user

---
+++ myprofile.js
@@ -0,0 +1,76 @@
+// This script fetches the user's profile from the server and displays it on the page.
+
+document.addEventListener('DOMContentLoaded', async (event) => { // Make the function async
+  const token = localStorage.getItem('token');
+  console.log(`token: ${token}`);
+  const payload = JSON.parse(atob(token.split('.')[1])); // Decode payload of JWT
+  console.log(payload);
+  const userId = payload.id;
+  console.log(`userId: ${userId}`);
+  if (!userId) {
+    displayNoProfileMessage();
+    return;
+  }
+  const config = {
+    headers: { 'Authorization': `Bearer ${token}` },
+    params: { userId: userId }
+  };
+  try {
+    const response = await axios.get(`https://cofounder-connect-d2057df29b96.herokuapp.com/cofounders/profile`, config);
+    const profile = response.data;
+    console.log(profile);
+    if (profile) {
+      displayProfile(profile);
+    } else {
+      displayNoProfileMessage();
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
+
+function displayNoProfileMessage() {
+  console.log('No profile found. Create a profile?');
+  const profileContainer = document.getElementById('profile-container');
+  profileContainer.innerHTML = `
+    <p>No profile found. Create a profile?</p>
+    <a href="/profile.html">Create Profile</a>
+  `;
+}
+
+function displayProfile(profile) {
+  const myProfile = profile[0];
+  console.log('Displaying profile:', profile);
+  console.log(myProfile.ideasInterestedIn);
+  const profileContainer = document.getElementById('profile-container')
+  profileContainer.innerHTML = `
+  ${myProfile.aboutMe}<br>
+  <br>
+  <strong>LinkedIn:</strong> <a href="${myProfile.linkedinURL}" target="_blank">${myProfile.linkedinURL}</a><br>
+  <strong>Program Type:</strong> ${myProfile.programType}<br>
+  <strong>Industry Interests:</strong> ${myProfile.industryInterests}<br>
+  <strong>Is Technical:</strong> ${myProfile.isTechnical ? 'Yes' : 'No'}<br>
+  <br>
+
+  <strong>Education:</strong><br>
+  ${myProfile.education}<br>
+  <strong>Employment History:</strong><br>
+  ${myProfile.employmentHistory}<br>
+  <strong>Impressive Accomplishment:</strong><br>
+  ${myProfile.impressiveAccomplishment}<br>
+  <br>
+
+  <strong>Has Idea:</strong> ${profile.hasIdea}<br>
+  <strong>Potential Ideas:</strong><br>
+  ${myProfile.ideasInterestedIn}<br>
+  <br>
+
+  <strong>My Ideal Cofounder:</strong> ${myProfile.cofounderDesiredQualities}<br>
+  <strong>Areas I Can Be Responsible For:</strong> ${myProfile.areasOfResponsibility}(', ')}<br>
+  <strong>Timeline For Getting Started:</strong> ${myProfile.timelineForFulltime}<br>
+  <br>
+
+  <strong>Email:</strong> ${myProfile.email}<br>
+  <strong>Scheduling URL:</strong> <a href="${myProfile.schedulingURL}" target="_blank">${myProfile.schedulingURL}</a><br>`
+}
+
